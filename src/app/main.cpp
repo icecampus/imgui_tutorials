bc913c080121305ee26dd81eef0789c8a68c2bcb
ice_rezzet <ice.rezzet@gmail.com>
2025-01-05 19:06:35 +0300

    add manifest

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <magic_enum/magic_enum.hpp>
 
+#include <boost/program_options/option.hpp>
+
+enum class Color : int{ RED1 = -10, BLUE = 0, GREEN = 10 };
 
 int main()
 {
-	std::cout << "Hello!!!";
+	std::cout << "Hello!!!\n";
+
+	Color c1 = Color::RED1;
+	std::cout << magic_enum::enum_name(c1) << std::endl; // RED
+
+	
 	return 0;
 }
